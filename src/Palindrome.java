aa0be3a52d2ae106d2c491006994fef05c88eb54
Aleksandr <aapopov812@gmail.com>
2018-10-11 03:36:15 +0300

    Initial commit

---
+++ src/Palindrome.java
@@ -0,0 +1,20 @@
+public class Palindrome {
+
+    public static void main(String[] args) {
+
+        String s = "Madam, I'm Adam!";
+        if (Palindrome.isPalindrome(s)) {
+            System.out.println("This is Palindrome");
+
+        } else {
+            System.out.println("This isn't Palindrome");
+        }
+
+    }
+    public static boolean isPalindrome(String s) {
+        return s.replaceAll("\\W","")
+                .equalsIgnoreCase(new StringBuilder(s.replaceAll("\\W",""))
+                        .reverse().toString());
+    }
+
+}
